879c037eff0e173cbe06f3cea5215896cc83288d
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-02-13 11:45:53 +0100

    lotto game in funcion and dockstring

--- lotto.py
+++ lotto.py
@@ -1,15 +1,19 @@
 import random
 
-user_numbers = []
-pc_numbers = list(range(1, 7))
-random.shuffle(pc_numbers)
-pc_numbers = pc_numbers[:6]
-correct_number = []
-
-for i in range(6):
-    user_number = input("Choose the number: ")
-    try:
-        user_number = int(user_number)
+def lotto():
+    """Allows to guess 6 numbers comparing to computer randomly chosen"""
+    user_numbers = []
+    pc_numbers = list(range(1, 50))
+    random.shuffle(pc_numbers)
+    pc_numbers = pc_numbers[:6]
+    correct_number = []
+    for i in range(6):
+        user_number = input("Choose the number: ")
+        try:
+            user_number = int(user_number)
+        except ValueError:
+            print("It's not a number. Try one more time")
+            break
         if user_number not in range(1, 50):
             print("Number outside range 1-49. Try one more time")
             break
@@ -18,14 +22,12 @@ for i in range(6):
             break
         else:
             user_numbers.append(user_number)
-    except ValueError:
-        print("It's not a number. Try one more time")
-        break
-for i in user_numbers:
-    if i in pc_numbers:
-        correct_number.append(i)
+    for i in user_numbers:
+        if i in pc_numbers:
+            correct_number.append(i)
+    user_numbers.sort()
+    print(f"Yor numbers: \n{user_numbers}")
+    print(f"Lotto numbers: \n{pc_numbers}")
+    print(f"You hit {len(correct_number)} number/s")
 
-user_numbers.sort()
-print(f"Yor numbers: \n{user_numbers}")
-print(f"Lotto numbers: \n{pc_numbers}")
-print(f"You hit {len(correct_number)} number/s")
+lotto()
