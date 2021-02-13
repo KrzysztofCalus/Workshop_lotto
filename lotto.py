0b826398b2f81f0835e67f89a66c95e0c78e5a8e
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-02-13 11:04:18 +0100

    lotto game without funcion and possibility to chose the same number by computer

---
+++ lotto.py
@@ -0,0 +1,30 @@
+import random
+
+user_numbers = []
+pc_numbers = []
+correct_number = []
+
+for i in range(6):
+    user_number = input("Choose the number: ")
+    try:
+        user_number = int(user_number)
+        if user_number not in range(1, 50):
+            print("Number outside range 1-49. Try one more time")
+            break
+        if user_number in user_numbers:
+            print("Number already added. Try one more time")
+            break
+        else:
+            user_numbers.append(user_number)
+            pc_numbers. append(random.randint(1, 50))
+    except ValueError:
+        print("It's not a number. Try one more time")
+        break
+for i in user_numbers:
+    if i in pc_numbers:
+        correct_number.append(i)
+
+user_numbers.sort()
+print(f"Yor numbers: \n{user_numbers}")
+print(f"Lotto numbers: \n{pc_numbers}")
+print(f"You hit {len(correct_number)} number/s")
